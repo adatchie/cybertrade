0871437692661b38f3c194bc103d083a2abfc735
adatchie <adatchie@gmail.com>
2025-11-22 17:11:48 +0900

    Fix syntax error in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react'
 import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vite.dev/config/
-plugins: [
-  react(),
-  basicSsl()
-]
+export default defineConfig({
+  plugins: [
+    react(),
+    basicSsl()
+  ]
 })
