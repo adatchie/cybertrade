1cc34410d09533e505a64b0ec4aeffb5dd4e3434
adatchie <adatchie@gmail.com>
2025-11-23 09:42:40 +0900

    Setup local Express server for scraping API

--- vite.config.ts
+++ vite.config.ts
@@ -7,5 +7,14 @@ export default defineConfig({
   plugins: [
     react(),
     basicSsl()
-  ]
+  ],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  }
 })
